e79830407b22fa4aadf011838559b8e02fc6f948
Sne <sneha.patel.infosec@gmail.com>
2026-09-20 19:26:21 +0530

    Update introToLua.lua

--- introToLua.lua
+++ introToLua.lua
@@ -10,3 +10,22 @@ gameVersion = 2.1
 temperature = -10
 print(playerScore)
 -- No shit this is literally python
+
+-- nil represents the absence of any value in Lua.      
+--Declaring a variable without assigning a value automatically sets it to nil:
+
+local emptyVariable --need to add local here otherwise it won't be recognized as a variable
+print(emptyVariable)  -- nil
+-- Use local to declare variables scoped to the current block.     
+--nil is distinct from 0, "", or false. It means no value has been assigned.    
+
+--The type() function returns the data type of a value or variable as a string:
+playerScore = 100
+playerName = "NENE"
+isReady = true
+emptyValue = nil
+
+print(type(playerScore))  -- outputs: number
+print(type(playerName))   -- outputs: string
+print(type(isReady))      -- outputs: boolean
+print(type(emptyValue))   -- outputs: nil
