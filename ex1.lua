fbb33337d3b23b90cf3caa4f888d867e0a92fa28
Sne <sneha.patel.infosec@gmail.com>
2026-09-21 20:07:22 +0530

    Create ex1.lua

---
+++ ex1.lua
@@ -0,0 +1,9 @@
+--[[
+Create variables to represent a complete game character profile using the appropriate data types for each piece of information:
+
+Create a variable named characterName and assign it the string "Warrior"
+Create a variable named characterLevel and assign it the number 12
+Create a variable named characterHealth and assign it the number 85
+Create a variable named isHero and assign it the boolean true
+After creating all variables, print each one on a separate line in the order they were created.
+]]
